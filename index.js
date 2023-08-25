cb2eb9fc805ccc78da66cb24bf31ae115ea947b0
Samuel Santana <theSamuelSantana@gmail.com>
2023-08-24 19:05:35 -0700

    Initial commit

--- index.js
+++ index.js
@@ -16,10 +16,30 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection == 'rock' && computerSelection == 'paper' ||
             playerSelection == 'paper' && computerSelection == 'scissors' ||
             playerSelection == 'scissors' && computerSelection == 'rock') {
+        winningSelection = playerSelection;
+        losingSelection = computerSelection;
         message += 'You Win!'
     } else {
+        winningSelection = computerSelection;
+        losingSelection = playerSelection;
         message += 'You Lose!'
     }
 
     return `${message} ${winningSelection} beats ${losingSelection}!`
+}
+
+function game() {
+    let playerSelection = prompt()
+    let computerSelection = getComputerChoice()
+
+    let message = playRound(playerSelection, computerSelection)
+
+    console.log(message)
+}
+
+var games = 5;
+while (games > 0){
+    console.log(`Games Remaining: ${games}`)
+    game()
+    games -= 1
 }
